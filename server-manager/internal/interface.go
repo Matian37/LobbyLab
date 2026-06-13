31549c323bef129b8032cde40ae40ed34394f7b8
Mateusz Pietrowcow <104150350+Matian37@users.noreply.github.com>
2026-06-13 15:35:38 +0200

    chore(server-manager): add interfaces

---
+++ server-manager/internal/interface.go
@@ -0,0 +1,40 @@
+package internal
+
+import (
+	"context"
+	"time"
+
+	"github.com/moby/moby/api/types/network"
+)
+
+type DockerClient interface {
+	Init(config *EnvConfig) error
+	CreateWorkerContainer(ctx context.Context) (string, error)
+	IsContainerOK(ctx context.Context, id string) (bool, error)
+	RestartContainer(ctx context.Context, id string) error
+	KillContainer(ctx context.Context, id string) error
+	GetPorts(ctx context.Context, containerID string) (network.PortMap, error)
+	Close() error
+}
+
+type BrokerConnection interface {
+	Open(timeout time.Duration, config *EnvConfig) error
+	SendPing(ctx context.Context) error
+	GetPong(ctx context.Context) (string, time.Time, error)
+	AssignJob(ctx context.Context, workerID string, config string) error
+	Close() error
+}
+
+type WorkerManager interface {
+	Init(ctx context.Context, config *EnvConfig, workerCount int) error
+	WaitForFreeWorker(ctx context.Context)
+	AssignMatch(ctx context.Context, config string) error
+	Monitor(ctx context.Context)
+	SaveResults(ctx context.Context)
+	Close(ctx context.Context)
+}
+
+type Matchmaker interface {
+	Init(ctx context.Context) error
+	Run(ctx context.Context)
+}
