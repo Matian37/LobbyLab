e33ec2d4ec8fa9ca8aa03f87a8d015a0c09cc0f6
Mateusz Pietrowcow <104150350+Matian37@users.noreply.github.com>
2026-07-26 13:57:00 +0200

    refactor(api): adjust api/results endpoint to newest db.js changes

--- api/src/routes/api/results/+server.js
+++ api/src/routes/api/results/+server.js
@@ -4,12 +4,11 @@ import { json } from '@sveltejs/kit';
 export async function GET({ cookies }) {
     const token = cookies.get('token');
     if (token == undefined) return json({ sukces: false, matches: null });
-    const dbLogin = await getLoginFromToken(token);
-    if (dbLogin.length == 0) {
+
+    const login = await getLoginFromToken(token);
+    if (login === null) {
         console.debug('nie istnieje sesja z danym tokenem');
         return json({ sukces: false, matches: null });
     }
-    const login = dbLogin[0].login;
-    const matches = await getMatchResults(login);
-    return json({ sukces: true, matches: matches });
+    return json({ sukces: true, matches: await getMatchResults(login) });
 }
