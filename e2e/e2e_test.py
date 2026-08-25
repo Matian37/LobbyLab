7bf61d9d791a41fe168db53ac7bdf459c19951ac
PASJANSS <kacper.wojtkielewiczz@gmail.com>
2026-08-25 13:18:02 +0200

    refactor(e2e): change e2e testfile's directory

--- e2e/e2e_test.py
+++ e2e/e2e_test.py
@@ -23,7 +23,7 @@ GAME_SERVER_COUNT = 2
 PLAYERS_PER_ROOM = 2
 GAME_SERVER_IMAGE = "game-server:latest"
 GAME_CLIENT_FILENAME = "game-client.zip"
-PROJECT_FOLDER = os.path.dirname(os.path.abspath(__file__))
+PROJECT_FOLDER = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 logger = logging.getLogger(__name__)
 
